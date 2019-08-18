1b869aedd55ce653de4c2ecb3cd468f8c9d0fce0
Jonas Spinner <jonas.spinner@student.kit.edu>
2019-08-18 18:29:01 +0200

    Adding ILP-solver

---
+++ cmake/modules/FindGUROBI.cmake
@@ -0,0 +1,78 @@
+#### Taken from https://raw.githubusercontent.com/joschu/trajopt/master/cmake/modules/FindGUROBI.cmake
+##### Who has it from:
+#### Taken from http://www.openflipper.org/svnrepo/CoMISo/trunk/CoMISo/cmake/FindGUROBI.cmake
+
+# License of trajopt:
+
+# Copyright (c) 2013, John Schulman
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# http://opensource.org/licenses/BSD-2-Clause
+
+
+# - Try to find GUROBI
+# Once done this will define
+#  GUROBI_FOUND - System has Gurobi
+#  GUROBI_INCLUDE_DIRS - The Gurobi include directories
+#  GUROBI_LIBRARIES - The libraries needed to use Gurobi
+
+if (GUROBI_INCLUDE_DIR)
+    # in cache already
+    set(GUROBI_FOUND TRUE)
+    set(GUROBI_INCLUDE_DIRS "${GUROBI_INCLUDE_DIR}" )
+    set(GUROBI_LIBRARIES "${GUROBI_LIBRARY};${GUROBI_CXX_LIBRARY}" )
+else (GUROBI_INCLUDE_DIR)
+
+    find_path(GUROBI_INCLUDE_DIR
+            NAMES gurobi_c++.h
+            PATHS "$ENV{GUROBI_HOME}/include"
+            )
+
+    find_library( GUROBI_LIBRARY
+            NAMES
+            gurobi
+            gurobi45
+            gurobi46
+            gurobi50
+            gurobi51
+            gurobi52
+            gurobi55
+            gurobi56
+            gurobi60
+            gurobi65
+            gurobi70
+            gurobi75
+            gurobi80
+            gurobi81
+            PATHS "$ENV{GUROBI_HOME}/lib"
+            )
+
+    find_library( GUROBI_CXX_LIBRARY
+            NAMES
+            gurobi_g++5.2
+            #gurobi_c++
+            PATHS "$ENV{GUROBI_HOME}/lib"
+            )
+
+    set(GUROBI_INCLUDE_DIRS "${GUROBI_INCLUDE_DIR}" )
+    set(GUROBI_LIBRARIES "${GUROBI_LIBRARY};${GUROBI_CXX_LIBRARY}" )
+
+    # use c++ headers as default
+    set(GUROBI_COMPILER_FLAGS "-DIL_STD" CACHE STRING "Gurobi Compiler Flags")
+
+    include(FindPackageHandleStandardArgs)
+    # handle the QUIETLY and REQUIRED arguments and set GUROBI_FOUND to TRUE
+    # if all listed variables are TRUE
+    find_package_handle_standard_args(GUROBI  DEFAULT_MSG
+            GUROBI_LIBRARY GUROBI_CXX_LIBRARY GUROBI_INCLUDE_DIR)
+
+    mark_as_advanced(GUROBI_INCLUDE_DIR GUROBI_LIBRARY GUROBI_CXX_LIBRARY)
+
+endif(GUROBI_INCLUDE_DIR)
