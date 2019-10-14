050d6b76d645e5e57a594784b1a37ca9432fbf7d
Jonas Spinner <jonas.spinner@student.kit.edu>
2019-10-14 22:43:19 +0200

    Added support for unweighted instances

--- scripts/graph_io.py
+++ scripts/graph_io.py
@@ -14,22 +14,35 @@ def read_metis_graph(path: Union[str, Path]) -> Tuple[nx.Graph, np.ndarray]:
     with path.open("r") as f:
         lines = f.read().split("\n")
 
-    n, m, fmt = map(int, lines[0].split(" "))
-    assert m == n * (n - 1) // 2
-    assert fmt == 1
-
+    n, m, fmt, *_ = list(map(int, lines[0].split(" "))) + [0]
     graph = nx.Graph()
     graph.name = path.name
     graph.add_nodes_from(range(n))
 
     S = np.zeros((n, n))
 
-    for u, line in enumerate(lines[1:]):
-        it = iter(line.split())
-        for v, c in [(int(v) - 1, float(c)) for (v, c) in zip(it, it)]:
-            if c >= 0:
-                graph.add_edge(u, v, cost=c)
-            S[u, v] = S[v, u] = c
+    if fmt == 0 or fmt is None:
+        # unweighted, default
+
+        S[:] = 1
+
+        for u, line in enumerate(lines[1:]):
+            for v in [int(v) - 1 for v in line.split()]:
+                graph.add_edge(u, v)
+
+    elif fmt == 1:
+        # weighted
+        assert m == n * (n - 1) // 2
+
+        for u, line in enumerate(lines[1:]):
+            it = iter(line.split())
+            for v, c in [(int(v) - 1, float(c)) for (v, c) in zip(it, it)]:
+                if c >= 0:
+                    graph.add_edge(u, v, cost=c)
+                S[u, v] = S[v, u] = c
+
+    else:
+        raise Exception(f"fmt \"{fmt}\" not supported")
 
     return graph, S
 
