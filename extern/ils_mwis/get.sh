4e5eaa523184d02f6d23983bc2990c4d111f0427
Jonas Spinner <jonas.spinner@student.kit.edu>
2020-01-28 14:48:00 +0100

    Added ils_mwis as external resource

---
+++ extern/ils_mwis/get.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd "$(dirname "$0")" || exit
+
+ILS_MWIS_DIR=.
+ILS_MWIS_DOWNLOAD_DIR=$ILS_MWIS_DIR/download
+
+mkdir -p $ILS_MWIS_DOWNLOAD_DIR
+
+curl -L -o $ILS_MWIS_DOWNLOAD_DIR/ils-mwis.tar.gz https://sites.google.com/site/nogueirabruno/software/ils-mwis.tar.gz
+tar -C $ILS_MWIS_DIR -xf $ILS_MWIS_DOWNLOAD_DIR/ils-mwis.tar.gz
+rm $ILS_MWIS_DOWNLOAD_DIR/ils-mwis.tar.gz
+
+patch -p1 -i ./*.patch
