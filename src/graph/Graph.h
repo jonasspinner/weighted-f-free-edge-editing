02453a00d2dd04fcf5c71e0f5e215d26934eefd1
Jonas Spinner <jonas.spinner@student.kit.edu>
2020-03-12 18:35:33 +0100

    Moved VertexPair into own file.

--- src/graph/Graph.h
+++ src/graph/Graph.h
@@ -11,42 +11,7 @@
 #include <boost/dynamic_bitset.hpp>
 #include <yaml-cpp/yaml.h>
 
-#include "../definitions.h"
-
-
-class VertexPair {
-public:
-    Vertex u;
-    Vertex v;
-
-    /**
-     * An pair of vertices. Can be used as an undirected edge.
-     *
-     * The vertex pairs {u, v} and {v, u} are the same object. It is guaranteed that pair.u < pair.v.
-     *
-     * @param x A vertex
-     * @param y A vertex
-     */
-    VertexPair(Vertex x, Vertex y) : u(x < y ? x : y), v(x < y ? y : x) { assert(x != y); }
-
-    friend std::ostream &operator<<(std::ostream &os, VertexPair uv) {
-        return os << "{" << uv.u << ", " << uv.v << "}";
-    }
-
-    friend YAML::Emitter &operator<<(YAML::Emitter &out, VertexPair uv) {
-        out << YAML::Flow;
-        out << YAML::BeginSeq << uv.u << uv.v << YAML::EndSeq;
-        return out;
-    }
-
-    friend bool operator==(const VertexPair &uv, const VertexPair &xy) {
-        return (uv.u == xy.u) && (uv.v == xy.v);
-    }
-
-    friend bool operator<(const VertexPair &uv, const VertexPair &xy) {
-        return uv.u < xy.u || (uv.u == xy.u && uv.v < xy.v);
-    }
-};
+#include "VertexPair.h"
 
 
 class Graph {
---
+++ src/graph/VertexPair.h
@@ -0,0 +1,47 @@
+//
+// Created by jonas on 12.03.20.
+//
+
+#ifndef WEIGHTED_F_FREE_EDGE_EDITING_VERTEXPAIR_H
+#define WEIGHTED_F_FREE_EDGE_EDITING_VERTEXPAIR_H
+
+
+#include "../definitions.h"
+
+
+class VertexPair {
+public:
+    Vertex u;
+    Vertex v;
+
+    /**
+     * An pair of vertices. Can be used as an undirected edge.
+     *
+     * The vertex pairs {u, v} and {v, u} are the same object. It is guaranteed that pair.u < pair.v.
+     *
+     * @param x A vertex
+     * @param y A vertex
+     */
+    VertexPair(Vertex x, Vertex y) : u(x < y ? x : y), v(x < y ? y : x) { assert(x != y); }
+
+    friend std::ostream &operator<<(std::ostream &os, VertexPair uv) {
+        return os << "{" << uv.u << ", " << uv.v << "}";
+    }
+
+    friend YAML::Emitter &operator<<(YAML::Emitter &out, VertexPair uv) {
+        out << YAML::Flow;
+        out << YAML::BeginSeq << uv.u << uv.v << YAML::EndSeq;
+        return out;
+    }
+
+    friend bool operator==(const VertexPair &uv, const VertexPair &xy) {
+        return (uv.u == xy.u) && (uv.v == xy.v);
+    }
+
+    friend bool operator<(const VertexPair &uv, const VertexPair &xy) {
+        return uv.u < xy.u || (uv.u == xy.u && uv.v < xy.v);
+    }
+};
+
+
+#endif //WEIGHTED_F_FREE_EDGE_EDITING_VERTEXPAIR_H
