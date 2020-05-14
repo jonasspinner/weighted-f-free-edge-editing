7fb09865387119b379242246b44ab2c35e2e1389
Jonas Spinner <jonas.spinner@student.kit.edu>
2020-05-14 16:51:59 +0200

    Modified WPLS recursion state.

--- src/lower_bound/weighted_packing/WeightedPackingLocalSearch.h
+++ src/lower_bound/weighted_packing/WeightedPackingLocalSearch.h
@@ -3,57 +3,38 @@
 
 #include "WeightedPacking.h"
 
+#include "robin_hood.h"
+#include <unordered_set>
+
 
 class WeightedPackingLocalSearch : public LowerBoundI {
-
-    const Graph &m_graph;
-    const VertexPairMap<bool> &m_marked;
-    const VertexPairMap<Cost> &m_costs;
-
     class State {
     public:
-        WeightedPacking m_packing;
-        std::vector<std::pair<Subgraph, Cost>> m_subgraphs_in_packing;
+        robin_hood::unordered_map<Subgraph, Cost> m_subgraphs_in_packing;
 
         [[nodiscard]] size_t num_subgraphs_in_packing() const {
             return m_subgraphs_in_packing.size();
         }
 
-        [[nodiscard]] const auto &subgraph_in_packing(size_t index) const {
-            return m_subgraphs_in_packing[index];
-        }
-
-        void remove(size_t index) {
-            std::swap(m_subgraphs_in_packing[index], m_subgraphs_in_packing.back());
-            const auto&[subgraph, cost] = m_subgraphs_in_packing.back();
-            m_packing.add_to_potential(subgraph, cost);
-            m_subgraphs_in_packing.pop_back();
-        }
-
-        void replace(size_t index, Subgraph &&subgraph, Cost cost) {
-            const auto&[old_subgraph, old_cost] = m_subgraphs_in_packing[index];
-            m_packing.add_to_potential(old_subgraph, old_cost);
-            m_packing.subtract_from_potential(subgraph, cost);
-            m_subgraphs_in_packing[index] = {std::move(subgraph), cost};
-        }
-
-        void add(Subgraph &&subgraph, Cost cost) {
-            m_packing.subtract_from_potential(subgraph, cost);
-            m_subgraphs_in_packing.emplace_back(std::move(subgraph), cost);
-        }
-
-        void add(std::pair<Subgraph, Cost> &&element) {
-            const auto&[subgraph, cost] = element;
-            m_packing.subtract_from_potential(subgraph, cost);
-            m_subgraphs_in_packing.push_back(std::move(element));
-        }
-
-        [[nodiscard]] bool is_valid() const {
-            return m_packing.is_valid();
+        [[nodiscard]] const auto &subgraphs() const {
+            return m_subgraphs_in_packing;
         }
     };
 
+    const Graph &m_graph;
+    const VertexPairMap<Cost> &m_costs;
+    const VertexPairMap<bool> &m_marked;
+
+    WeightedPacking m_packing;
     std::vector<std::unique_ptr<State>> m_states;
+
+public:
+    WeightedPackingLocalSearch(const Instance &instance, const VertexPairMap<bool> &marked,
+                               const SubgraphStats &subgraph_stats, std::shared_ptr<FinderI> finder_ref) :
+            LowerBoundI(std::move(finder_ref)), m_graph(instance.graph), m_costs(instance.costs), m_marked(marked),
+            m_packing(instance, marked, subgraph_stats) {
+        m_states.emplace_back();
+    }
 
     State &current_state() {
         assert(!m_states.empty());
@@ -69,7 +50,7 @@ class WeightedPackingLocalSearch : public LowerBoundI {
         auto &state = current_state();
         greedy_initialize(state, k);
         local_search(state);
-        return state.m_packing.cost();
+        return m_packing.cost();
     }
 
     void local_search(State &state) {
@@ -79,22 +60,20 @@ class WeightedPackingLocalSearch : public LowerBoundI {
 
         for (size_t iter = 0; iter < max_iter && num_rounds_no_improvement < max_rounds_no_improvement; ++iter) {
             bool found_improvement = false;
-            for (size_t index = 0; index < state.m_subgraphs_in_packing.size(); ++index) {
-                found_improvement |= find_one_two_improvement(state, index);
+            for (const auto &[subgraph, cost] : state.subgraphs()) {
+                found_improvement |= find_one_two_improvement(state, subgraph, cost);
             }
             num_rounds_no_improvement = found_improvement ? 0 : num_rounds_no_improvement + 1;
         }
     }
 
     bool greedy_initialize(State &state, Cost k) {
-        auto &packing = state.m_packing;
-
         std::vector<std::pair<Subgraph, Cost>> subgraph_heap;
         Cost lower_bound = 0;
         Cost max_min_cost = std::numeric_limits<Cost>::min();
 
         bool unsolvable = finder->find_with_duplicates(m_graph, [&](Subgraph &&subgraph) {
-            Cost initial_min_cost = packing.calculate_min_cost(subgraph);
+            Cost initial_min_cost = m_packing.calculate_min_cost(subgraph);
 
             subgraph_heap.emplace_back(std::move(subgraph), initial_min_cost);
 
@@ -111,7 +90,7 @@ class WeightedPackingLocalSearch : public LowerBoundI {
             std::pop_heap(subgraph_heap.begin(), subgraph_heap.end());
             const auto &[subgraph, cost] = subgraph_heap.back();
 
-            Cost current_min_cost = packing.calculate_min_cost(subgraph);
+            Cost current_min_cost = m_packing.calculate_min_cost(subgraph);
 
             if (current_min_cost == 0) {
                 // The subgraph cannot be inserted.
@@ -121,7 +100,9 @@ class WeightedPackingLocalSearch : public LowerBoundI {
                 // and update the remaining cost matrix.
                 lower_bound += current_min_cost;
 
-                state.add(std::move(subgraph_heap.back()));
+                m_packing.subtract_from_potential(subgraph, cost);
+                state.m_subgraphs_in_packing.emplace(std::move(subgraph_heap.back()));
+
                 subgraph_heap.pop_back();
             } else {
                 // The editing cost is no longer up to date. The subgraph may be inserted in the future.
@@ -130,74 +111,75 @@ class WeightedPackingLocalSearch : public LowerBoundI {
             }
         }
 
-        assert(is_maximal());
+        assert(m_packing.is_maximal());
         return true;
     }
 
 
-    bool find_one_two_improvement(State &state, size_t index) {
-        auto &packing = state.m_packing;
+    bool find_one_two_improvement(State &state, const Subgraph &x, Cost x_cost) {
+        m_packing.add_to_potential(x, x_cost);
 
-        const auto &[x, x_cost] = state.subgraph_in_packing(index);
-
-        packing.add_to_potential(x, x_cost);
-
-
-        const auto pairs = packing.get_neighbor_pairs(x);
-        auto[candidates, border] = packing.get_neighbors(pairs);
+        const auto pairs = m_packing.get_neighbor_pairs(x);
+        auto[candidates, border] = m_packing.get_neighbors(pairs);
 
         // The information about the best solution.
+        // Remark: the subgraph x will always be a candidate.
         Cost max_cost = x_cost;
-        std::vector<std::pair<size_t, Cost>> max_i;
+        std::vector<std::vector<std::pair<size_t, Cost>>> max_i;
 
 
         for (size_t pair_i = 0; pair_i < pairs.size(); ++pair_i) {
             for (size_t a_i = border[pair_i]; a_i < border[pair_i + 1]; ++a_i) {
                 const Subgraph &a = candidates[a_i];
 
-                auto a_cost = packing.calculate_min_cost(a);
-                packing.subtract_from_potential(a, a_cost);
+                auto a_cost = m_packing.calculate_min_cost(a);
+                m_packing.subtract_from_potential(a, a_cost);
 
                 if (a_cost > max_cost) {
                     max_cost = a_cost;
-                    max_i = {{a_i, a_cost}};
+                    max_i = {{{a_i, a_cost}}};
+                } else if (a_cost == max_cost) {
+                    max_i.push_back({{a_i, a_cost}});
                 }
 
                 for (size_t pair_j = pair_i + 1; pair_j < pairs.size(); ++pair_j) {
-                    if (packing.is_depleted(pairs[pair_j]))
+                    if (m_packing.is_depleted(pairs[pair_j]))
                         continue;
                     for (size_t b_i = border[pair_j]; b_i < border[pair_j + 1]; ++b_i) {
                         const Subgraph &b = candidates[b_i];
 
-                        auto b_cost = packing.calculate_min_cost(b);
+                        auto b_cost = m_packing.calculate_min_cost(b);
 
                         if (b_cost > 0) {
                             // b can be inserted.
 
                             if (a_cost + b_cost > max_cost) {
                                 max_cost = a_cost + b_cost;
-                                max_i = {{a_i, a_cost},
-                                         {b_i, b_cost}};
+                                max_i = {{{a_i, a_cost}, {b_i, b_cost}}};
+                            } else if (a_cost + b_cost == max_cost) {
+                                max_i.push_back({{a_i, a_cost}, {b_i, b_cost}});
                             }
                         }
                     }
                 }
 
-                packing.add_to_potential(a, a_cost);
+                m_packing.add_to_potential(a, a_cost);
             }
         }
 
         if (max_i.empty()) {
             // No improvement has been found. x is still optimal.
-            packing.subtract_from_potential(x, x_cost);
-        } else if (max_cost == x_cost) {
-            // Plateau search
+            m_packing.subtract_from_potential(x, x_cost);
+            // TODO: Should not happen, as x is also a candidate.
         } else {
-            for (auto[y_i, y_cost] : max_i)
-                packing.subtract_from_potential(candidates[y_i], y_cost);
+            // Plateau search
+            size_t j = 0; // currently first element.
+            // TODO: Plateau search strategy.
+            for (auto[y_i, y_cost] : max_i[j])
+                m_packing.subtract_from_potential(candidates[y_i], y_cost);
         }
 
-        assert(packing.is_valid());
+        assert(m_packing.is_valid());
         return max_cost > x_cost;
     }
 
