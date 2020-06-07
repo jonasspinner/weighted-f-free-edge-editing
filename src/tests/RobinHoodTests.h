9f1b3062d693a939bb81b9598de9bcb495046f96
Jonas Spinner <jonas.spinner@student.kit.edu>
2020-06-07 17:16:48 +0200

    Ignored seed parameter.

--- src/tests/RobinHoodTests.h
+++ src/tests/RobinHoodTests.h
@@ -7,7 +7,7 @@
 
 class RobinHoodTests {
 public:
-    RobinHoodTests(int seed) {}
+    RobinHoodTests(int /*seed*/) {}
 
     void run() {
         std::cout << "\nRobinHoodTests"
@@ -17,6 +17,23 @@ public:
 
         Subgraph a{0, 1, 2, 3};
         map[std::move(a)] = 24;
+
+        for (const auto&[k, v] : map) {
+            std::cout << k << " : " << v << "\n";
+        }
+
+        std::vector<std::pair<Subgraph, Cost>> subgraphs{{Subgraph{0, 1, 2}, 10}, {Subgraph{3, 4, 5}, 20}, {Subgraph{6, 7, 8}, 30}};
+        for (const auto& [subgraph, cost] : subgraphs) {
+            std::cout << "(" << subgraph << " , " << cost << ") ";
+        }
+        std::cout << "\n";
+        map.emplace(std::move(subgraphs.back()));
+        subgraphs.pop_back();
+        map.insert(robin_hood::pair(std::move(subgraphs.back())));
+        for (const auto& [subgraph, cost] : subgraphs) {
+            std::cout << "(" << subgraph << " , " << cost << ") ";
+        }
+        std::cout << "\n";
 
         for (const auto&[k, v] : map) {
             std::cout << k << " : " << v << "\n";
