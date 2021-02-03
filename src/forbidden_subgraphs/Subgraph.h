5c7573f9b235447fbe840380a6384f87a392c7f8
Jonas Spinner <jonas.spinner@student.kit.edu>
2021-02-03 11:20:53 +0100

    Removed generic SubgraphT<> definition.

--- src/forbidden_subgraphs/Subgraph.h
+++ src/forbidden_subgraphs/Subgraph.h
@@ -8,34 +8,7 @@
 
 
 template<Options::FSG SetOfForbiddenSubgraphs>
-class SubgraphT {
-    struct SubgraphFinder{};
-    class VertexIt {};
-    class VertexPairIt {};
-    class NonConvertingEdits {};
-    using Subgraph = SubgraphT<SetOfForbiddenSubgraphs>;
-public:
-    using Finder = SubgraphFinder;
-
-    SubgraphT() {
-        throw std::runtime_error("unimplemented");
-    }
-
-    VertexIt vertices() const;
-    VertexPairIt vertex_pairs() const;
-    NonConvertingEdits non_converting_edits() const;
-    Cost calculate_min_cost(const VertexPairMap<Cost> &costs, const VertexPairMap<bool> &marked) const;
-    [[nodiscard]] bool operator==(const Subgraph &other) const;
-    [[nodiscard]] bool operator!=(const Subgraph &other) const;
-    [[nodiscard]] bool operator<(const Subgraph &other) const;
-    [[nodiscard]] bool contains(Vertex u) const;
-    [[nodiscard]] bool contains(VertexPair uv) const;
-
-    template<Options::FSG Set>
-    friend std::ostream &operator<<(std::ostream &os, const SubgraphT<Set> &subgraph);
-    template<Options::FSG Set>
-    friend YAML::Emitter &operator<<(YAML::Emitter &out, const SubgraphT<Set> &subgraph);
-};
+class SubgraphT;
 
 
 namespace subgraph_iterators {
